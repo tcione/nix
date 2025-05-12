c60ca3d20f6163eb759835ac7d9406cb1f0f1cf9
Tales Cione <talesj@gmail.com>
2025-05-12 22:51:14 +0200

    remove flash from sundial

--- user-config/sundial.nix
+++ user-config/sundial.nix
@@ -19,13 +19,23 @@
           TIME=$(date +%H%M)
           SUNRISE=$(echo "$TZ_DATA" | jq '.results | .sunrise')
           SUNSET=$(echo "$TZ_DATA" | jq '.results | .sunset')
+          STATE_PATH="$HOME/.sundial-temperature"
 
-          pkill -ef "hyprsunset -t" || true
-
-          if [[ "$TIME" > "$SUNSET" ]] || [[ "$TIME" < "$SUNRISE" ]]; then
-            hyprctl keyword exec "hyprsunset -t 2800"
+          if [[ -f "$STATE_PATH" ]]; then
+            LAST_TEMPERATURE=$(cat "$STATE_PATH")
           else
-            hyprctl keyword exec "hyprsunset -t 6000"
+            LAST_TEMPERATURE="undefined"
+          fi
+
+          NEW_TEMPERATURE="6000"
+          if [[ "$TIME" > "$SUNSET" ]] || [[ "$TIME" < "$SUNRISE" ]]; then
+            NEW_TEMPERATURE="2800"
+          fi
+
+          if [[ $LAST_TEMPERATURE != $NEW_TEMPERATURE ]]; then
+            # pkill -ef "hyprsunset -t" || true
+            echo "$NEW_TEMPERATURE" > "$STATE_PATH"
+            hyprctl keyword exec "hyprsunset -t $NEW_TEMPERATURE"
           fi
 
           exit 0
