b179f9f72b4aacc7b435aa0fbceb702ea2d7804e
Tales Cione <talesj@gmail.com>
2025-08-08 20:03:59 +0200

    sleepy-turtle: new wallpaper

--- user-config/hyprpaper.nix
+++ user-config/hyprpaper.nix
@@ -2,15 +2,14 @@
 
 {
   services.hyprpaper.enable = true;
-  services.hyprpaper.settings.splash = true;
+  services.hyprpaper.settings.splash = false;
   services.hyprpaper.settings.splash_offset = 2.0;
   services.hyprpaper.settings.ipc = "on";
   services.hyprpaper.settings.preload = [
-    "~/Pictures/Backgrounds/flores.jpg"
-    "~/Pictures/Backgrounds/waves1.jpg"
+    "~/Pictures/Backgrounds/a_cat_in_a_cup.png"
+    "~/Pictures/Backgrounds/a_cartoon_of_a_island_with_a_bridge_and_trees_01.jpg"
   ];
   services.hyprpaper.settings.wallpaper = [
-    ",~/Pictures/Backgrounds/flores.jpg"
-    ",~/Pictures/Backgrounds/waves1.jpg"
+    ",~/Pictures/Backgrounds/a_cartoon_of_a_island_with_a_bridge_and_trees_01.jpg"
   ];
 }
