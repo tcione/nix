a75ac3d771a2472286a3fb09d85e898876958113
Tales Cione <talesj@gmail.com>
2025-08-12 21:31:59 +0200

    waybar: add weekday

--- user-config/waybar.nix
+++ user-config/waybar.nix
@@ -66,7 +66,7 @@
         };
         clock = {
           tooltip-format = "<tt>{calendar}</tt>";
-          format = "{:%H:%M}";
+          format = "{:%A, %H:%M}";
           format-alt = "{:%Y-%m-%d %H:%M:%S}";
           calendar = {
             mode = "year";
