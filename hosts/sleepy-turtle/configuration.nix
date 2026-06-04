e4a61af209ac6f0e2929e7cc1ead47600c778b8e
Tales Cione <talesj@gmail.com>
2026-06-04 22:18:18 +0200

    wireplumber: migrate bluez config from lua to SPA-JSON

--- hosts/sleepy-turtle/configuration.nix
+++ hosts/sleepy-turtle/configuration.nix
@@ -98,16 +98,14 @@
   services.pipewire.alsa.support32Bit = true;
   services.pipewire.enable = true;
   services.pipewire.pulse.enable = true;
-  services.pipewire.wireplumber.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '')
-  ];
+  services.pipewire.wireplumber.extraConfig."51-bluez" = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+    };
+  };
   services.printing.enable = true;
   services.upower.enable = true;
 
